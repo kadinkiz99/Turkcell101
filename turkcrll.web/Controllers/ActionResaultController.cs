fdb0184b02040a7e1f79d09d01969975f8d8444d
Kadınkız Önal <gazze4488@gmail.com>
2022-10-28 00:06:17 +0300

    Action Resault çeşitleri

---
+++ turkcrll.web/Controllers/ActionResaultController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace turkcell.web.Controllers
+{
+	public class ActionResaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		// sayfada kontrol ederken == /Actionresault/contentresault
+		//geriye string değer döndürmek için kullanılır 
+		public IActionResult ContentResault()
+		{
+
+			return Content("Content Resault");
+		}
+
+		// geriye json çıktısı dönderir.
+		//Actionresault/jsonetresault
+		public IActionResult JsonResault()
+		{
+
+			return Json(new { Id = 3 , name="Kalem" , Price = 200});
+		}
+
+		// geriye boş sayfa  dönderir.
+		///Actionresault/emptyresault
+		public IActionResult EmptyResault()
+		{
+
+			return new EmptyResult();
+		}
+	}
+}
